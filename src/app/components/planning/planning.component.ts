757a4170f6651bb11eec10b116406c3e814c0365
Akela <haouamyassine@gmail.com>
2023-02-22 16:29:24 +0100

    v1

--- src/app/components/planning/planning.component.ts
+++ src/app/components/planning/planning.component.ts
@@ -7,21 +7,15 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   templateUrl: './planning.component.html',
   styleUrls: ['./planning.component.css']
 })
+
+
 export class PlanningComponent {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
-    // insertion d'event en dur
-    // dateClick: this.handleDateClick.bind(this), // MUST ensure `this` context is maintained
-
-
-    // events: [
-    //   { title: 'event 1', date: '2023-03-05' },
-    //   { title: 'event 2', date: '2023-03-06' }
-    // ]
+    plugins: [dayGridPlugin]
   };
 
-  // handleDateClick(arg) {
-  //   alert('date click! ' + arg.dateStr)
-  // }
-}
+
+
+
+  }
