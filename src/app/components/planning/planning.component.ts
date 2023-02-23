5bc53cda6817c6c742c52c230db48c1139693992
Akela <haouamyassine@gmail.com>
2023-02-23 11:31:12 +0100

    fullcalendar-add-del-events
    pas encore de lien avec l'api
    only en dur

--- src/app/components/planning/planning.component.ts
+++ src/app/components/planning/planning.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {CalendarOptions, DateSelectArg, EventApi, EventClickArg} from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import {createEventId, INITIAL_EVENTS} from "./event-utils";
+// import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 @Component({
   selector: 'app-planning',
@@ -11,11 +16,68 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 export class PlanningComponent {
   calendarOptions: CalendarOptions = {
+    plugins: [
+      dayGridPlugin,
+      interactionPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+  },
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
   };
 
+  currentEvents: EventApi[] = [];
 
-
-
+  constructor(private changeDetector: ChangeDetectorRef) {
   }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    this.changeDetector.detectChanges();
+  }
+
+
+
+
+
+}
