c3bea2a979abb4e845c102b3ea9b3b1931dd4762
sauvageb <sauvageboris.pro@gmail.com>
2023-02-28 09:52:43 +0100

    weather: remove unused service import

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,36 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
-import {map} from "rxjs";
-import { UsersService } from "./users.service";
 @Injectable()
 export class WeatherService {
 
   url = 'https://api.openweathermap.org/data/2.5/weather';
   apiKey = '0096b809430b4da65543e5a54a4c149e';
 
+  constructor(public http: HttpClient) {
+  }
 
-  constructor(public http: HttpClient,
-  private usersService: UsersService) {
-    console.log('open weather service connected');
-   }
+  getWeatherDataByCords({lat, lon}: { lat: any, lon: any }) {
+    let params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('units', 'metric')
+      .set('appid', this.apiKey)
 
-      getWeatherDataByCords({lat, lon}: { lat: any, lon: any }){
-        let params = new HttpParams()
-          .set('lat', lat)
-          .set('lon', lon)
-          .set('units', 'metric')
-          .set('appid', this.apiKey)
+    return this.http.get(this.url, {params});
+  }
 
-        return this.http.get(this.url, { params });
-      }
+  getWeatherDataByCityName(city: string) {
+    let params = new HttpParams()
+      .set('q', city)
+      .set('units', 'metric')
+      .set('appid', this.apiKey)
 
-      getWeatherDataByCityName(city: string){
-        let params= new HttpParams()
-          .set('q', city)
-          .set('units', 'metric')
-          .set('appid', this.apiKey)
-
-        return this.http.get(this.url, { params });
-      }
+    return this.http.get(this.url, {params});
+  }
 }
